a9952a449c0e702178ee6e29b502894882195e81
bewatermyfriend <49046655+karata-erika@users.noreply.github.com>
2024-10-31 17:25:06 +0900

    게시판

---
+++ src/test/java/com/nyeonjae/advancedbbs/AdvancedBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyeonjae.advancedbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
